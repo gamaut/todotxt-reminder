687fd9f22e006636d521ab38ce7cd231bd2b87fb
Γ/ut <thuyje@protonmail.com>
2024-06-27 02:13:00 +0200

    Create todotxt-reminder.sh

---
+++ todotxt-reminder.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TODO_FILE="todo.txt"
+TODAY=$(date +%Y-%m-%d)
+TOMORROW=$(date -d tomorrow +%Y-%m-%d)
+
+generate_list() {
+    local context
+
+    mapfile -t contexts < <(
+        grep -v "^x " "$TODO_FILE" | awk -v today="$TODAY" -v tomorrow="$TOMORROW" '
+        /due:/ {
+            due_date = gensub(/.*due:([0-9]{4}-[0-9]{2}-[0-9]{2}).*/, "\\1", "g", $0)
+            if (due_date <= today || due_date == tomorrow) {
+                match($0, /@[^\s]+/, arr)
+                if (arr[0] != "") print arr[0]
+            }
+        }
+        ' | sort -u
+    )
+
+    for context in "${contexts[@]}"; do
+        echo
+        echo "$context"
+
+        grep -v "^x " "$TODO_FILE" | grep "$context" | awk -v today="$TODAY" -v tomorrow="$TOMORROW" '
+            /due:/ {
+                due_date = gensub(/.*due:([0-9]{4}-[0-9]{2}-[0-9]{2}).*/, "\\1", "g", $0)
+                if (due_date <= today || due_date == tomorrow) print
+            }
+        '
+    done
+}
+
+generate_list
